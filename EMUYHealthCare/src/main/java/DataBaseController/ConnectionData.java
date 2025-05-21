cb39666bdec744071dad2fe5cfe62a2da7da5f92
shintanach <shintalyaa06@gmail.com>
2025-05-21 23:02:58 +0700

    Database Users
    membuat database users
    membuat insert data

--- EMUYHealthCare/src/main/java/DataBaseController/ConnectionData.java
+++ EMUYHealthCare/src/main/java/DataBaseController/ConnectionData.java
@@ -2,8 +2,13 @@ package DataBaseController;
 
 public abstract class ConnectionData {
     private final String USER_DATA = "jdbc:sqlite:EMUYHealthCare/Database/User/UserData.db";
+    private final String INSERT_DATA ="INSERT INTO \"users\" VALUES (NULL,?, ?, ?, ?, ?)";
 
     public String getUserData() {
         return USER_DATA;
+    }
+
+    public String getINSERT_DATA(){
+        return INSERT_DATA;
     }
 }
