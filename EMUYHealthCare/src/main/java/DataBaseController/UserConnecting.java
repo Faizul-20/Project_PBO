419e599d600764b51ae776e188eb9363261753ca
Faizul_Mushofa <faizulmushofa1@gmail.com>
2025-06-04 21:37:04 +0700

    OK
    ganti data

--- EMUYHealthCare/src/main/java/DataBaseController/UserConnecting.java
+++ EMUYHealthCare/src/main/java/DataBaseController/UserConnecting.java
@@ -1,14 +1,10 @@
 package DataBaseController;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class UserConnecting extends ConnectionData implements SQLConnection {
-    private final String INSERT_DATA ="INSERT INTO \"users\" VALUES (NULL,?, ?, ?, ?, ?)";
 
     @Override
     public void ConnectToDatabase(String Url) {
@@ -26,6 +22,19 @@ public class UserConnecting extends ConnectionData implements SQLConnection {
     public void InsertDataUser(String username, String password, int tb, int bb, String ttl) {
         try {
             Connection connection = DriverManager.getConnection(getUserData());
+
+            String checkQuery = "SELECT * FROM users WHERE Username = ?";
+            PreparedStatement CheckStmt = connection.prepareStatement(checkQuery);
+            CheckStmt.setString(1,username);
+            ResultSet CheckResult = CheckStmt.executeQuery();
+
+            if (CheckResult.next()){
+                System.out.println("Username '" + username + "' sudah terdaftar. Gunakan username lain!");
+                CheckResult.close();
+                CheckStmt.close();
+                connection.close();
+            }
+
             String query = getINSERT_DATA();
             PreparedStatement pstmt = connection.prepareStatement(query);
 
@@ -46,9 +55,34 @@ public class UserConnecting extends ConnectionData implements SQLConnection {
         }
     }
 
-    public String getINSERT_DATA() {
-        return INSERT_DATA;
+    public void SignIn(String username,String password){
+
+        try {
+            Connection connection =  DriverManager.getConnection(getUserData());
+            String login = getSIGN_IN();
+            PreparedStatement pstmt = connection.prepareStatement(login);
+
+            pstmt.setString(1,username);
+            pstmt.setString(2,password);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next() ) {
+                System.out.println("Login berhasil! Selamat datang, " + username + "!");
+            } else {
+                System.out.println("Username atau password salah.");
+
+            }
+
+            pstmt.close();
+            connection.close();
+
+        }catch (SQLException e){
+            System.out.println("Pesan Eror : " + e.getMessage());
+        }
+
     }
+
 }
 
 
