eeb3f4b18c56e1bb970b8c7b40ed1e863218cf9c
shintanach <shintalyaa06@gmail.com>
2025-06-04 21:01:29 +0700

    Menambahkan database
    menambahkan method insertdatauser dan sign in, menghapus database userdata

--- EMUYHealthCare/src/main/java/DataBaseController/ConnectionData.java
+++ EMUYHealthCare/src/main/java/DataBaseController/ConnectionData.java
@@ -1,9 +1,10 @@
 package DataBaseController;
 
 public abstract class ConnectionData {
-    private final String USER_DATA = "jdbc:sqlite:EMUYHealthCare/Database/User/UserData.db";
+    private final String USER_DATA = "jdbc:sqlite:EMUYHealthCare/Database/User/Users.db";
     private final String PENYAKIT_DATA = "jdbc:sqlite:EMUYHealthCare/Database/Engine Data/DaftarPenyakit.db";
-
+    private final String INSERT_DATA ="INSERT INTO users VALUES (NULL,?, ?, ?, ?, ?)";
+    private final String SIGN_IN ="SELECT * FROM users WHERE Username = ? AND Password = ?";
 
 
     public String getUserData() {
@@ -12,5 +13,13 @@ public abstract class ConnectionData {
 
     public String getPENYAKIT_DATA() {
         return PENYAKIT_DATA;
+    }
+
+    public String getINSERT_DATA(){
+        return INSERT_DATA;
+    }
+
+    public String getSIGN_IN() {
+        return SIGN_IN;
     }
 }
